a953a3966ee10c2c4ef2d54aea048571660694ef
Alibehzad79 <Alibehzad13791122@gmail.com>
2024-11-16 10:06:54 +0400

    add duration and currenttime in player

--- components/Player.vue
+++ components/Player.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useTemplateRef } from 'vue'
-
+import { useMediaControls } from '@vueuse/core'
 const props = defineProps([
     "src",
     'image',
@@ -38,7 +38,17 @@ const muted = () => {
         player.value.volume = 0
     }
 }
-
+const time_convert = (num) => {
+    var hours = Math.floor(num / 60);
+    var minutes = num % 60;
+    if (hours < 10) {
+        hours = '0' + hours
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    }
+    return hours + ":" + minutes;
+}
 </script>
 
 <template>
@@ -58,25 +68,29 @@ const muted = () => {
             </div>
             <div class="flex flex-col gap-5 justify-between w-full">
                 <div class="flex items-center gap-10 mx-auto">
+                    <span class="text-white">{{ time_convert(Math.floor(range)) }}</span>
                     <UIcon name="solar:play-line-duotone" size="35" class="cursor-pointer text-white"
                         :class="{ 'text-yellow-400': played, 'text-white': !played }" @click="play" />
                     <UIcon name="solar:pause-line-duotone" size="35" class="cursor-pointer text-white"
                         :class="{ 'text-yellow-400': paused, 'text-white': !paused }" @click="pause" />
                     <UIcon name="solar:repeat-one-bold" size="35" class="cursor-pointer text-white"
                         :class="{ 'text-yellow-400': loop, 'text-white': !loop }" @click="loop = !loop" />
+                    <span class="text-white">{{ time_convert(Math.floor(player?.duration)) }}</span>
                 </div>
                 <URange v-model="range" :min="0" v-on:update:model-value="player.currentTime = range"
                     :max="player?.duration" color="yellow" size="sm" />
             </div>
-            <div class="w-full lg:w-1/2 flex flex-row lg:flex-col items-center gap-5">
+            <div class="w-full lg:w-1/2 flex flex-row lg:flex-col items-center justify-center gap-5">
                 <div class="flex items-center gap-2 w-1/2">
                     <UIcon name="solar:muted-bold" size="35" class="cursor-pointer text-white"
                         :class="{ 'text-yellow-400': mute, 'text-white': !mute }" @click="muted" />
                     <URange v-model="volume" v-on:update:model-value="player.volume = volume / 100" :min="0" :max="100"
                         color="yellow" size="sm" />
                 </div>
-                <UButton label="Download" color="yellow" variant="outline" class="w-1/2 justify-center"
-                    icon="solar:download-square-bold" />
+                <a :href="src" download
+                    class="text-yellow-400 flex items-center gap-2 text-sm border border-yellow-400 p-1 rounded lg:w-1/2 justify-center">
+                    <Icon name="solar:download-square-bold" /> Download
+                </a>
             </div>
         </div>
     </div>
